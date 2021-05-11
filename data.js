7f7028fb6d713089b8ae2fd3087e686c7e3e6e49
flame <76920703+flamebousteur@users.noreply.github.com>
2021-05-11 21:41:27 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,50 @@
+var data = {
+	"craftS+":{
+		"desc":"New craft for minecraft survival.",
+		"descfr":"De nouveau craft pour votre survie.",
+		"type":"data pack",
+		"photo":2
+	},
+	"shield":{
+		"desc":"green glass + concrete powder = the best sheld 👍<br><br><br>1- set the concrete powder 2 blocks around the green glass.<br>2- go on the green glass.3- 25 armor stand was summon.<br>4- and 3 light blue glass was set over the concrete powder.<br>5- you can break the shields with snowballs.<br><br>good game",
+		"descfr":"green glass + concrete powder = the best sheld 👍<br><br>1- met le béton blanc 2 blocks autour du verre vert.<br>2- vas sur le verre vert.<br>4- 3 verre bleu claire son générer sur les béton bleu.<br>5- tu peux casser le bouclier avec des boule de neige.<br><br>bonne partie",
+		"type":"data pack",
+		"photo":5
+	},
+	"spawner-generator":{
+		"desc":"5 gold block + 1 netherite block + 1 end crystal = spawner👍\n\n\n\nhi if you’ve always dreamed to have a spawner this data pack it's made for you.\n\n1- download this data pack.\n2- build the structure.\n3- a new spawner is here!\n\ngood game. 😁",
+		"descfr":"5 gold block + 1 netherite block + 1 end crystal = spawner👍\n\n\nsi tu as toujours révé d'avoir un spawner ce datapack est fait pour toi!\n\n1- télécharge ce datapack.\n2- costruit la structure.\n3- un nouveau spawner!\n\namusez vous bien. 😁",
+		"type":"data pack",
+		"photo":3
+	},
+	"the-floor-is-lava":{
+		"desc":"just the floor is lava no more good luck 😉.",
+		"descfr":"le sol c'est de la lave donc BONNE CHANCE!.",
+		"type":"data pack",
+		"photo":0
+	},
+	"utility-item":{
+		"desc":"no def.",
+		"descfr":"no def.",
+		"type":"data pack",
+		"photo":0
+	},
+	"cove-in-minecraft":{
+		"desc":"",
+		"descfr":"",
+		"type":"map",
+		"photo":2
+	},
+	"floating-island":{
+		"desc":"",
+		"descfr":"",
+		"type":"map",
+		"photo":2
+	},
+	"wisard-tower":{
+		"desc":"",
+		"descfr":"",
+		"type":"map",
+		"photo":2
+	}
+}
---
+++ script.js
@@ -0,0 +1,51 @@
+if(screen.width < 400){
+	let dpmap = document.getElementById("dp-map");
+	dpmap.style.textAlign = "center";
+}
+
+if(screen.width < 51){
+	if(screen.width < 51){
+		console.log("nano 🔍")
+	}
+}
+
+function copi(txt){
+	navigator.clipboard.writeText(txt)
+	msg("copy to clipboard")
+}
+
+function msg(txt,time){
+	if(txt == ''){
+		var txt = 'undefined'
+	}
+	if(typeof time != 'undefined'){
+		var time = time * 1000
+	}else{
+		var time = 1000
+	}
+	let msg = document.getElementById("msg")
+	msg.style.opacity = "1";
+	msg.innerHTML = txt
+	window.setTimeout(msgp, time);		
+	function msgp(){
+		document.getElementById("msg").style.opacity = "0";
+	}
+	delete txt
+	delete time
+}
+
+function $_GET(param) {
+	var vars = {};
+	window.location.href.replace( location.hash, '' ).replace( 
+		/[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+		function( m, key, value ) { // callback
+			vars[key] = value !== undefined ? value : '';
+		}
+	);
+
+	if ( param ) {
+		return vars[param] ? vars[param] : null;	
+	}
+	return vars;
+}
+var $_GET = $_GET()
