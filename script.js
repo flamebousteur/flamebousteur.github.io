23dd9b3a559dc43782947bc4d50621732223da83
flame <76920703+flamebousteur@users.noreply.github.com>
2021-05-11 11:52:14 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+if(screen.width < 400){
+	let dpmap = document.getElementById("dp-map");
+	dpmap.style.textAlign = "center";
+}
+
+if(screen.width < 51){
+	if(screen.width < 51){
+		console.log("nano 🔍")
+	}
+}
+
+function copi(fnc){
+	navigator.clipboard.writeText(fnc)
+	msg("copy to clipboard")
+}
+
+function msg(txt,time){
+	if(typeof time != 'undefined'){
+		var time = time * 1000
+	}else{
+		var time = 1000
+	}
+	let msg = document.getElementById("msg")
+	msg.style.opacity = "1";
+	msg.innerHTML = txt
+	window.setTimeout(msgp, time);		
+	function msgp(){
+		document.getElementById("msg").style.opacity = "0";
+	}
+	delete time
+}
