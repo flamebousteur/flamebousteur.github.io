465b937f1b37c9671c956fe1836f51e5462969ea
flame <76920703+flamebousteur@users.noreply.github.com>
2021-06-20 16:51:51 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,12 @@
 const data = {
+	"modern-office":{
+		"desc":"",
+		"descfr":"",
+		"type":"map",
+		"photo":0,
+		"color":"#999999",
+		"video":"nxw75apm6bs"
+	},
 	"floating-island":{
 		"desc":"",
 		"descfr":"",
@@ -23,20 +31,12 @@ const data = {
 		"color":"#462100",
 		"video":"ge1WMCKf-yA"
 	},
-	"modern-office":{
-		"desc":"",
-		"descfr":"",
-		"type":"map",
-		"photo":0,
-		"color":"#999999",
-		"video":"nxw75apm6bs"
-	},
-	"craftS+":{
-		"desc":"New craft for minecraft survival.",
-		"descfr":"De nouveau craft pour votre survie.",
+	"spawner-generator":{
+		"desc":"5 gold block + 1 netherite block + 1 end crystal = spawner👍\n\n\n\nhi if you’ve always dreamed to have a spawner this data pack it's made for you.\n\n1- download this data pack.\n2- build the structure.\n3- a new spawner is here!\n\ngood game. 😁",
+		"descfr":"5 gold block + 1 netherite block + 1 end crystal = spawner👍\n\n\nsi tu as toujours révé d'avoir un spawner ce datapack est fait pour toi!\n\n1- télécharge ce datapack.\n2- costruit la structure.\n3- un nouveau spawner!\n\namusez vous bien. 😁",
 		"type":"data pack",
-		"photo":2,
-		"color":"#38e8ee"
+		"photo":3,
+		"color":"#f7ff00"
 	},
 	"shield":{
 		"desc":"green glass + concrete powder = the best sheld 👍<br><br><br>1- set the concrete powder 2 blocks around the green glass.<br>2- go on the green glass.3- 25 armor stand was summon.<br>4- and 3 light blue glass was set over the concrete powder.<br>5- you can break the shields with snowballs.<br><br>good game",
@@ -45,12 +45,19 @@ const data = {
 		"photo":5,
 		"color":"#3893ee"
 	},
-	"spawner-generator":{
-		"desc":"5 gold block + 1 netherite block + 1 end crystal = spawner👍\n\n\n\nhi if you’ve always dreamed to have a spawner this data pack it's made for you.\n\n1- download this data pack.\n2- build the structure.\n3- a new spawner is here!\n\ngood game. 😁",
-		"descfr":"5 gold block + 1 netherite block + 1 end crystal = spawner👍\n\n\nsi tu as toujours révé d'avoir un spawner ce datapack est fait pour toi!\n\n1- télécharge ce datapack.\n2- costruit la structure.\n3- un nouveau spawner!\n\namusez vous bien. 😁",
+	"utility-item":{
+		"desc":"no def.",
+		"descfr":"no def.",
 		"type":"data pack",
-		"photo":3,
-		"color":"#f7ff00"
+		"photo":0,
+		"color":"#0e9408"
+	},
+	"craftS+":{
+		"desc":"New craft for minecraft survival.",
+		"descfr":"De nouveau craft pour votre survie.",
+		"type":"data pack",
+		"photo":2,
+		"color":"#38e8ee"
 	},
 	"the-floor-is-lava":{
 		"desc":"just the floor is lava no more good luck 😉.",
@@ -58,12 +65,5 @@ const data = {
 		"type":"data pack",
 		"photo":0,
 		"color":"#ec1d1d"
-	},
-	"utility-item":{
-		"desc":"no def.",
-		"descfr":"no def.",
-		"type":"data pack",
-		"photo":0,
-		"color":"#0e9408"
 	}
 }
