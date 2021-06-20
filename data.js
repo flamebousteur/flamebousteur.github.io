a8c243ec67d43ce2a0a1198d66dda67f8d7d2818
flame <76920703+flamebousteur@users.noreply.github.com>
2021-06-20 16:35:13 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,47 +1,4 @@
 const data = {
-	"the-floor-is-lava":{
-		"desc":"just the floor is lava no more good luck 😉.",
-		"descfr":"le sol c'est de la lave donc BONNE CHANCE!.",
-		"type":"data pack",
-		"photo":0,
-		"color":"#ec1d1d"
-	},
-	"craftS+":{
-		"desc":"New craft for minecraft survival.",
-		"descfr":"De nouveau craft pour votre survie.",
-		"type":"data pack",
-		"photo":2,
-		"color":"#38e8ee"
-	},
-	"utility-item":{
-		"desc":"no def.",
-		"descfr":"no def.",
-		"type":"data pack",
-		"photo":0,
-		"color":"#0e9408"
-	},
-	"shield":{
-		"desc":"green glass + concrete powder = the best sheld 👍<br><br><br>1- set the concrete powder 2 blocks around the green glass.<br>2- go on the green glass.3- 25 armor stand was summon.<br>4- and 3 light blue glass was set over the concrete powder.<br>5- you can break the shields with snowballs.<br><br>good game",
-		"descfr":"green glass + concrete powder = the best sheld 👍<br><br>1- met le béton blanc 2 blocks autour du verre vert.<br>2- vas sur le verre vert.<br>4- 3 verre bleu claire son générer sur les béton bleu.<br>5- tu peux casser le bouclier avec des boule de neige.<br><br>bonne partie",
-		"type":"data pack",
-		"photo":5,
-		"color":"#3893ee"
-	},
-	"spawner-generator":{
-		"desc":"5 gold block + 1 netherite block + 1 end crystal = spawner👍\n\n\n\nhi if you’ve always dreamed to have a spawner this data pack it's made for you.\n\n1- download this data pack.\n2- build the structure.\n3- a new spawner is here!\n\ngood game. 😁",
-		"descfr":"5 gold block + 1 netherite block + 1 end crystal = spawner👍\n\n\nsi tu as toujours révé d'avoir un spawner ce datapack est fait pour toi!\n\n1- télécharge ce datapack.\n2- costruit la structure.\n3- un nouveau spawner!\n\namusez vous bien. 😁",
-		"type":"data pack",
-		"photo":3,
-		"color":"#f7ff00"
-	},
-	"modern-office":{
-		"desc":"",
-		"descfr":"",
-		"type":"map",
-		"photo":0,
-		"color":"#999999",
-		"video":"nxw75apm6bs"
-	},
 	"floating-island":{
 		"desc":"",
 		"descfr":"",
@@ -65,5 +22,48 @@ const data = {
 		"photo":2,
 		"color":"#462100",
 		"video":"ge1WMCKf-yA"
+	},
+	"modern-office":{
+		"desc":"",
+		"descfr":"",
+		"type":"map",
+		"photo":0,
+		"color":"#999999",
+		"video":"nxw75apm6bs"
+	},
+	"craftS+":{
+		"desc":"New craft for minecraft survival.",
+		"descfr":"De nouveau craft pour votre survie.",
+		"type":"data pack",
+		"photo":2,
+		"color":"#38e8ee"
+	},
+	"shield":{
+		"desc":"green glass + concrete powder = the best sheld 👍<br><br><br>1- set the concrete powder 2 blocks around the green glass.<br>2- go on the green glass.3- 25 armor stand was summon.<br>4- and 3 light blue glass was set over the concrete powder.<br>5- you can break the shields with snowballs.<br><br>good game",
+		"descfr":"green glass + concrete powder = the best sheld 👍<br><br>1- met le béton blanc 2 blocks autour du verre vert.<br>2- vas sur le verre vert.<br>4- 3 verre bleu claire son générer sur les béton bleu.<br>5- tu peux casser le bouclier avec des boule de neige.<br><br>bonne partie",
+		"type":"data pack",
+		"photo":5,
+		"color":"#3893ee"
+	},
+	"spawner-generator":{
+		"desc":"5 gold block + 1 netherite block + 1 end crystal = spawner👍\n\n\n\nhi if you’ve always dreamed to have a spawner this data pack it's made for you.\n\n1- download this data pack.\n2- build the structure.\n3- a new spawner is here!\n\ngood game. 😁",
+		"descfr":"5 gold block + 1 netherite block + 1 end crystal = spawner👍\n\n\nsi tu as toujours révé d'avoir un spawner ce datapack est fait pour toi!\n\n1- télécharge ce datapack.\n2- costruit la structure.\n3- un nouveau spawner!\n\namusez vous bien. 😁",
+		"type":"data pack",
+		"photo":3,
+		"color":"#f7ff00"
+	},
+	"the-floor-is-lava":{
+		"desc":"just the floor is lava no more good luck 😉.",
+		"descfr":"le sol c'est de la lave donc BONNE CHANCE!.",
+		"type":"data pack",
+		"photo":0,
+		"color":"#ec1d1d"
+	},
+	"utility-item":{
+		"desc":"no def.",
+		"descfr":"no def.",
+		"type":"data pack",
+		"photo":0,
+		"color":"#0e9408"
 	}
 }
