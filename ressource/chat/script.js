cff9962e094e882b9be9042bbe0b4639a3228a69
flame <76920703+flamebousteur@users.noreply.github.com>
2021-07-24 17:00:15 +0200

    Update script.js

--- ressource/chat/script.js
+++ ressource/chat/script.js
@@ -9,7 +9,7 @@ const statlink = "https://www.fjmessgeraete.ch/59d71404-d59e-11eb-b8bc-0242ac130
 
 function chec(){
 	let xhr = new XMLHttpRequest();
-	xhr.open("POST", statlink+"/chat/index.php", true);
+	xhr.open("POST", statlink+"/chat/chat.php", true);
 	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 	xhr.send();
 }
@@ -34,7 +34,7 @@ function send(msg){
 			}else{
 				msg = document.getElementById("message").value
 			}
-			xhr.open("POST", statlink+"/chat/index.php/?m=in", true);
+			xhr.open("POST", statlink+"/chat/chat.php/?m=in", true);
 			xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 			xhr.send("msg="+msg);
 			tchatint = false;
@@ -45,7 +45,7 @@ function send(msg){
 
 function upchannel(){
 	let xhr = new XMLHttpRequest();
-	xhr.open("GET", statlink+"/chat/index.php/?m=out", true);
+	xhr.open("GET", statlink+"/chat/chat.php/?m=out", true);
 	xhr.onreadystatechange = function(){
 		if (xhr.readyState==4 && xhr.status==200){
 			let json = JSON.parse(xhr.responseText)
